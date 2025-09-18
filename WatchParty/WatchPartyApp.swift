c7925a664d8ae2c3f07063a4f524fbc22ba3e35a
jfeng <jerry.feng@alc.ca>
2025-09-18 11:06:10 -0300

    Initial Commit

---
+++ WatchParty/WatchPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchPartyApp.swift
+//  WatchParty
+//
+//  Created by Jerry Feng on 2025-09-18.
+//
+
+import SwiftUI
+
+@main
+struct WatchPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchPartyTests/WatchPartyTests.swift
@@ -0,0 +1,16 @@
+//
+//  WatchPartyTests.swift
+//  WatchPartyTests
+//
+//  Created by Jerry Feng on 2025-09-18.
+//
+
+import Testing
+
+struct WatchPartyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
